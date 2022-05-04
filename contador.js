db6172fc6fdbb09fbc5fa69f1b6c4ed6e2f4494f
ikersnc <ikersanchez51@gmail.com>
2022-05-04 13:43:38 +0200

    cambia color cada vez que desciende el contador

--- contador.js
+++ contador.js
@@ -5,11 +5,12 @@ let descenso = contador.innerHTML;
 let intervalo = setInterval(function() {
 
     descenso = descenso > 0 ? descenso - 1 : 0;
-    
+
     contador.innerHTML = descenso;
     contador.style.fontSize = descenso * 100 + 'px';
 
-    fondo.style.width = descenso * 10 + '%';
+    fondo.style.backgroundColor = `rgb( ${Math.floor(Math.random() * 255)} , ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+    console.log(fondo.style.backgroundColor);
     
     //DETENER INTERVALO AL LLEGAR A 0
     if(descenso <= 0) clearInterval(intervalo);
