6c253361d525c74ff7b6e4ceeb8009f4799269ec
ikersnc <ikersanchez51@gmail.com>
2022-05-05 16:40:16 +0200

    efecto transicion

--- bateria/bateria.js
+++ bateria/bateria.js
@@ -1,5 +1,5 @@
 //BOTONES
-let btn = document.getElementsByClassName('btn');
+let btn = document.querySelectorAll('.btn');
 //
 
 //AUDIOS
@@ -19,31 +19,40 @@ document.body.addEventListener('keypress', function(e) {
     let keyCode = e.key;
 
     switch(keyCode){
-        case 'a': console.log(keyCode);
+        case 'a':
+                console.log(keyCode);
                 clap.play();
                 break;
-        case 's': console.log(keyCode);
+        case 's':
+                console.log(keyCode);
                 hihat.play();
                 break;
-        case 'd': console.log(keyCode);
+        case 'd':
+                console.log(keyCode);
                 kick.play();
                 break;
-        case 'f': console.log(keyCode);
+        case 'f':
+                console.log(keyCode);
                 openhat.play();
                 break;
-        case 'g': console.log(keyCode);
+        case 'g':
+                console.log(keyCode);
                 boom.play();
                 break;
-        case 'h': console.log(keyCode);
+        case 'h':
+                console.log(keyCode);
                 ride.play();
                 break;
-        case 'j': console.log(keyCode);
+        case 'j':
+                console.log(keyCode);
                 snare.play();
                 break;
-        case 'k': console.log(keyCode);
+        case 'k':
+                console.log(keyCode);
                 tom.play();
                 break;
-        case 'l': console.log(keyCode);
+        case 'l':
+                console.log(keyCode);
                 tink.play();
                 break;
         default: break;
@@ -58,38 +67,47 @@ document.body.addEventListener('keydown', function(e) {
 
     switch(keyCode){
         case 'a':
+                btn[0].style.transition = 'all 200ms ease-out';
                 btn[0].style.boxShadow = '0 0 0 5px lightblue';
                 btn[0].style.transform = 'scale(110%, 110%)';
                 break;
         case 's':
+                btn[1].style.transition = 'all 200ms ease-out';
                 btn[1].style.boxShadow = '0 0 0 5px lightblue';
                 btn[1].style.transform = 'scale(110%, 110%)';
                 break;
         case 'd':
+                btn[2].style.transition = 'all 200ms ease-out';
                 btn[2].style.boxShadow = '0 0 0 5px lightblue';
                 btn[2].style.transform = 'scale(110%, 110%)';
                 break;
         case 'f':
+                btn[3].style.transition = 'all 200ms ease-out';
                 btn[3].style.boxShadow = '0 0 0 5px lightblue';
                 btn[3].style.transform = 'scale(110%, 110%)';
                 break;
         case 'g':
+                btn[4].style.transition = 'all 200ms ease-out';
                 btn[4].style.boxShadow = '0 0 0 5px lightblue';
                 btn[4].style.transform = 'scale(110%, 110%)';
                 break;
         case 'h':
+                btn[5].style.transition = 'all 200ms ease-out';
                 btn[5].style.boxShadow = '0 0 0 5px lightblue';
                 btn[5].style.transform = 'scale(110%, 110%)';
                 break;
         case 'j':
+                btn[6].style.transition = 'all 200ms ease-out';
                 btn[6].style.boxShadow = '0 0 0 5px lightblue';
                 btn[6].style.transform = 'scale(110%, 110%)';
                 break;
         case 'k':
+                btn[7].style.transition = 'all 200ms ease-out';
                 btn[7].style.boxShadow = '0 0 0 5px lightblue';
                 btn[7].style.transform = 'scale(110%, 110%)';
                 break;
         case 'l':
+                btn[8].style.transition = 'all 200ms ease-out';
                 btn[8].style.boxShadow = '0 0 0 5px lightblue';
                 btn[8].style.transform = 'scale(110%, 110%)';
                 break;
