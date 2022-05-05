2901f256f11530c2565fcd51b5777083d7e07fc7
ikersnc <ikersanchez51@gmail.com>
2022-05-05 13:27:35 +0200

    js detecta tecla pulsada

---
+++ bateria/bateria.js
@@ -0,0 +1,26 @@
+document.body.addEventListener('keydown', function(e) {
+    let keyCode = e.key;
+
+    switch(keyCode){
+        case 'a': console.log(keyCode);
+                break;
+        case 's': console.log(keyCode);
+                break;
+        case 'd': console.log(keyCode);
+                break;
+        case 'f': console.log(keyCode);
+                break;
+        case 'g': console.log(keyCode);
+                break;
+        case 'h': console.log(keyCode);
+                break;
+        case 'j': console.log(keyCode);
+                break;
+        case 'k': console.log(keyCode);
+                break;
+        case 'l': console.log(keyCode);
+                break;
+        default: break;
+    };
+
+});
