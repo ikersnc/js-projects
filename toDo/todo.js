f7f51618227ee12a7e9ffb0f0e1b04da713ae926
ikersnc <ikersanchez51@gmail.com>
2022-05-09 15:58:11 +0200

    create tasks

--- toDo/todo.js
+++ toDo/todo.js
@@ -1,14 +1,20 @@
-let addTask = document.getElementById('addTask');
+let enterTask = document.getElementById('enterTask');
+let cont = 0;
 
 let newTask = function() {
-
+    //ELEMENTOS
     let container = document.createElement('div');
     let tarea = document.createElement('input');
-
+    let texto = document.createElement('label');
+    //PROPIEDADES
     container.classList.add('tareas');
     tarea.type = 'checkbox';
+    tarea.id = 'tarea' + cont;
+    texto.htmlFor = tarea.id;
+    texto.innerText = enterTask.value;
     container.appendChild(tarea);
-    
+    container.appendChild(texto);
+    cont++;
     console.log(container);
 };
 
