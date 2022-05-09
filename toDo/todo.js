9299c55c62c5bbb958a0c160d7ac36e6c01c46c1
ikersnc <ikersanchez51@gmail.com>
2022-05-09 18:08:16 +0200

    error message and optimization

--- toDo/todo.js
+++ toDo/todo.js
@@ -1,26 +1,30 @@
-let enterTask = document.getElementById('enterTask');
 let main = document.getElementsByTagName('main');
 let tasks = document.getElementsByClassName('tareas');
 let cont = 0;
 
 addTask.addEventListener('click', function() {
-    //ELEMENTOS
-    let container = document.createElement('div');
-    let tarea = document.createElement('input');
-    let texto = document.createElement('label');
-    //PROPIEDADES
-    container.classList.add('tareas');
-    container.appendChild(tarea);
-    container.appendChild(texto);
-
-    tarea.type = 'checkbox';
-    tarea.id = 'tarea' + cont;
-
-    texto.htmlFor = tarea.id;
-    texto.innerText = enterTask.value;
-    //CONT PARA PROX. ID Y REINICIAR INPUT TAREA
-    cont++;
-    enterTask.value = '';
-    main[0].insertBefore(container, tasks[0]);
-    console.log(container, tasks[0]);
+    
+    if(enterTask.value === '')
+        error.innerText = 'Introduce una tarea';
+    else {
+        error.innerText = '';
+        //ELEMENTOS
+        let container = document.createElement('div');
+        let tarea = document.createElement('input');
+        let texto = document.createElement('label');
+        //PROPIEDADES
+        container.classList.add('tareas');
+        container.appendChild(tarea);
+        container.appendChild(texto);
+        
+        tarea.type = 'checkbox';
+        tarea.id = 'tarea' + cont;
+        
+        texto.htmlFor = tarea.id;
+        texto.innerText = enterTask.value;
+        //CONT PARA PROX. ID Y REINICIAR INPUT TAREA
+        cont++;
+        enterTask.value = '';
+        main[0].insertBefore(container, tasks[0]);
+    }
 });
