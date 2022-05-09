8874d7e03bb83cf011524739d79cd9aa6f0d5cc7
ikersnc <ikersanchez51@gmail.com>
2022-05-09 18:54:28 +0200

    submit at clicking enter

--- toDo/todo.js
+++ toDo/todo.js
@@ -2,29 +2,35 @@ let main = document.getElementsByTagName('main');
 let tasks = document.getElementsByClassName('tareas');
 let cont = 0;
 
-addTask.addEventListener('click', function() {
+function add() {
     
     if(enterTask.value === '')
-        error.innerText = 'Introduce una tarea';
+        error.innerText = 'Enter task';
     else {
         error.innerText = '';
-        //ELEMENTOS
+        //ELEMENTS
         let container = document.createElement('div');
         let tarea = document.createElement('input');
         let texto = document.createElement('label');
-        //PROPIEDADES
+        //PROPERTIES
         container.classList.add('tareas');
         container.appendChild(tarea);
         container.appendChild(texto);
-        
+
         tarea.type = 'checkbox';
         tarea.id = 'tarea' + cont;
-        
+
         texto.htmlFor = tarea.id;
         texto.innerText = enterTask.value;
-        //CONT PARA PROX. ID Y REINICIAR INPUT TAREA
+        //CONT FOR PROX. ID & REBOOT INPUT
         cont++;
         enterTask.value = '';
         main[0].insertBefore(container, tasks[0]);
     }
+};
+//ADD CLICKING BUTTON
+addTask.addEventListener('click', add);
+//ADD PRESSING ENTER
+enterTask.addEventListener('keyup', function(e) {
+    if(e.key === 'Enter') add();
 });
