7a749efa8018bcc5e07ef02ccdcd69b39a870dda
ikersnc <ikersanchez51@gmail.com>
2022-05-10 11:21:10 +0200

    styling and no tasks message

--- toDo/todo.js
+++ toDo/todo.js
@@ -1,5 +1,6 @@
 let main = document.getElementsByTagName('main');
 let tasks = document.getElementsByClassName('tareas');
+let noTasks = document.getElementById('noTasks');
 let cont = 0;
 
 function add() {
@@ -8,20 +9,25 @@ function add() {
         error.innerText = 'Enter task';
     else {
         error.innerText = '';
+        noTasks.classList.toggle('hidden');
         //ELEMENTS
         let container = document.createElement('div');
         let tarea = document.createElement('input');
         let texto = document.createElement('label');
+        let icon = document.createElement('li');
         //PROPERTIES
         container.classList.add('tareas');
         container.appendChild(tarea);
         container.appendChild(texto);
+        container.appendChild(icon);
 
         tarea.type = 'checkbox';
         tarea.id = 'tarea' + cont;
 
         texto.htmlFor = tarea.id;
         texto.innerText = enterTask.value;
+
+        icon.classList.add('fa-solid', 'fa-trash');
         //CONT FOR PROX. ID & REBOOT INPUT
         cont++;
         enterTask.value = '';
