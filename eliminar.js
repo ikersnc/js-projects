c88dde711b40094b342961be156d2211e47ae9d8
ikersnc <ikersanchez51@gmail.com>
2022-05-13 17:10:13 +0200

    button deletes itself

--- eliminar.js
+++ eliminar.js
@@ -1,3 +1,4 @@
 let toDeleteClass = document.getElementsByClassName('toDeleteClass');
 
-btn.addEventListener('click', () => toDeleteClass[0].remove());
+//btn.addEventListener('click', () => toDeleteClass[0].remove());
+btn.addEventListener('click', () => btn.remove());
