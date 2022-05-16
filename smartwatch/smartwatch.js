4f5c70a46f29b8c4750fab858bca59077c524076
ikersnc <ikersanchez51@gmail.com>
2022-05-16 18:48:18 +0200

    initial js added

---
+++ smartwatch/smartwatch.js
@@ -0,0 +1,5 @@
+blue.addEventListener('click', () => console.log('funciona'));
+red.addEventListener('click', () => console.log('funciona'));
+black.addEventListener('click', () => console.log('funciona'));
+purple.addEventListener('click', () => console.log('funciona'));
+pink.addEventListener('click', () => console.log('funciona'));
